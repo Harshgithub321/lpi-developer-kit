ef29e7fbfdc514e39920bbcbda64466aa3bee887
Rahul Bijarnia <rahul.bijarnia@s.amity.edu>
2026-04-20 14:04:38 +0530

    Delete submissions/Rahul_Bijarnia directory

--- submissions/Rahul_Bijarnia/tools.py
+++
@@ -1,5 +0,0 @@
-def lpi_tool_1(query):
-    return f"[Tool1] Documentation about {query}: Linear regression is a supervised learning algorithm used for prediction."
-
-def lpi_tool_2(query):
-    return f"[Tool2] Example: Equation y = mx + c represents {query}"
